9c4a75d04539b116af94da3bfb746477bb6559fb
peckpeck20 <peckpeck20@users.noreply.github.com>
2017-09-22 21:46:10 +0300

    First Commit

---
+++ src/test/java/task_2/Chapter_2_2/Chapter22ApplicationTests.java
@@ -0,0 +1,16 @@
+package task_2.Chapter_2_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Chapter22ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
