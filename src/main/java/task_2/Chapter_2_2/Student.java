f3227c113426383a3b3e9fdebd28d4dcea26c75d
peckpeck20 <peckpeck20@users.noreply.github.com>
2017-09-22 23:15:22 +0300

    Created Student Object and hello.html template

---
+++ src/main/java/task_2/Chapter_2_2/Student.java
@@ -0,0 +1,33 @@
+package task_2.Chapter_2_2;
+
+public class Student {
+	private String firstName;
+	private String lastName;
+	private String email;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+}
